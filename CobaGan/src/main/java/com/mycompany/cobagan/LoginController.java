afd9b02e58c886542dea041b0ea493c11e5820a2
Jonathan Billy <jonathan.billy@ti.ukdw.ac.id>
2020-11-09 15:53:04 +0700

    Log In
    Log In, Log Out

--- CobaGan/src/main/java/com/mycompany/cobagan/LoginController.java
+++ CobaGan/src/main/java/com/mycompany/cobagan/LoginController.java
@@ -66,16 +66,16 @@ public class LoginController implements Initializable {
             DBUtil.nim = rs.getString("nim");
             DBUtil.role = rs.getInt("role");
             if (role == 1) {
-                Parent root = FXMLLoader.load(getClass().getResource("/fxml/Scene.fxml"));
+                Parent root = FXMLLoader.load(getClass().getResource("/fxml/DashboardAdmin.fxml"));
                 Scene scene = new Scene(root);
-                scene.getStylesheets().add("/styles/Styles.css");
+                scene.getStylesheets().add("/styles/dashboardadmin.css");
                 Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
                 window.setScene(scene);
                 window.show();
             } else if(role == 2){
                 Parent root = FXMLLoader.load(getClass().getResource("/fxml/dashboardUser.fxml"));
                 Scene scene = new Scene(root);
-                scene.getStylesheets().add("/styles/Styles.css");
+                scene.getStylesheets().add("/styles/dashboardadmin.css");
                 Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
                 window.setScene(scene);
                 window.show();
