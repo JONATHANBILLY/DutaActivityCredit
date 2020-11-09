2484871e67e9c9286330aece6b2b0d66cb8ab3e8
Jonathan Billy <jonathan.billy@ti.ukdw.ac.id>
2020-11-09 15:53:04 +0700

    Log In
    Log In, Log Out

---
+++ CobaGan/src/main/java/com/mycompany/cobagan/DashboardAdminController.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.cobagan;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author billy
+ */
+public class DashboardAdminController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void keluar(ActionEvent event) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Login.fxml"));
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add("/styles/login.css");
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(scene);
+        window.show();
+    }
+
+    @FXML
+    private void lhtmhs(ActionEvent event) {
+    }
+
+    @FXML
+    private void tmbhmhs(ActionEvent event) {
+    }
+    
+}
