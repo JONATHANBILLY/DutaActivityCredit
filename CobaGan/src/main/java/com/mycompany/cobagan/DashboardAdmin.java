a5db687a82d6a5f97dfc99a13d8f66b14158a3dd
Jonathan Billy <jonathan.billy@ti.ukdw.ac.id>
2020-11-17 18:26:03 +0700

    query data
    query

--- CobaGan/src/main/java/com/mycompany/cobagan/DashboardAdmin.java
+++ CobaGan/src/main/java/com/mycompany/cobagan/DashboardAdmin.java
@@ -14,9 +14,38 @@ public class DashboardAdmin {
     private String nama;
     private String password;
     
-    public DashboardAdmin(String username, String nama, String password){
-        
+    public DashboardAdmin(String user, String name, String pass){
+        this.username = user;
+        this.password = pass;
+        this.nama = name;
     }
-    
+
+    DashboardAdmin(String username, String nama) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
     
 }
