636db379c3601f97c4d175c43dff8341081a232a
eka <eka.yuliet@ti.ukdw.ac.id>
2020-11-24 21:37:22 +0700

    delete unused data

--- CobaGan/src/main/java/com/mycompany/cobagan/Models/UserModel.java
+++ CobaGan/src/main/java/com/mycompany/cobagan/Models/UserModel.java
@@ -6,6 +6,7 @@
 package com.mycompany.cobagan.Models;
 
 import com.mycompany.cobagan.DBUtil;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -13,29 +14,34 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.stage.Stage;
 
 /**
  *
  * @author eka
  */
-
-
 public class UserModel {
 
     private String nama;
     private String nim;
     private Button update;
     private Button delete;
-    
-    public UserModel(){
+
+    public UserModel() {
     }
-    
-    public UserModel(String nama, String nim){
-     this.nama = nama;
-     this.nim = nim;
+
+    public UserModel(String nama, String nim) {
+        this.nama = nama;
+        this.nim = nim;
     }
- 
+
     public String getNama() {
         return nama;
     }
@@ -60,7 +66,7 @@ public class UserModel {
     public void setNim(String nim) {
         this.nim = nim;
     }
-    
+
     /**
      * @return the update
      */
@@ -73,20 +79,18 @@ public class UserModel {
      */
     public void setUpdate(Button update) {
         this.update = update;
-        
+
         update.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                String query = "UPDATE user SET nama="+nama+" where username =" + nim;
+                String query = "UPDATE user SET nama=" + nama + " where username =" + nim;
                 System.out.println(query);
                 Connection con = DBUtil.connect();
                 Statement stmt = null;
-                try{
-                stmt = con.createStatement();
-                stmt.executeUpdate(query);
-                }
-                catch (SQLException ex)
-                {
+                try {
+                    stmt = con.createStatement();
+                    stmt.executeUpdate(query);
+                } catch (SQLException ex) {
                 }
             }
         });
@@ -104,23 +108,31 @@ public class UserModel {
      */
     public void setDelete(Button delete) {
         this.delete = delete;
-         delete.setOnAction(new EventHandler<ActionEvent>() {
-    
+        delete.setOnAction(new EventHandler<ActionEvent>() {
+
             @Override
             public void handle(ActionEvent event) {
                 String query = "DELETE FROM user where username =" + nim;
                 System.out.println(query);
                 Connection con = DBUtil.connect();
                 Statement stmt = null;
-                try{
-                stmt = con.createStatement();
-                stmt.executeUpdate(query);
-                }
-                catch (SQLException ex)
-                {
+                try {
+                    stmt = con.createStatement();
+                    stmt.executeUpdate(query);
+                    Alert alert_p = new Alert(Alert.AlertType.INFORMATION, "Data Berhasil Dihapus!", ButtonType.YES);
+                    alert_p.showAndWait();
+                    Parent root = FXMLLoader.load(getClass().getResource("/fxml/DashboardAdmin.fxml"));
+                    Scene scene = new Scene(root);
+                    scene.getStylesheets().add("/styles/dashboardadmin.css");
+                    Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                    window.setScene(scene);
+                    window.show();
+                } catch (SQLException ex) {
+                } catch (IOException ex) {
+                    Logger.getLogger(UserModel.class.getName()).log(Level.SEVERE, null, ex);
                 }
             }
         });
     }
-    
+
 }
